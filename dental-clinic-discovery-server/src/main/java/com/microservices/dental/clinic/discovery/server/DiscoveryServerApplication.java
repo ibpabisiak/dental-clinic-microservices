19b5ec500df36705423e35c3795170064d2b0401
Bartlomiej Pabisiak <ibpabisiak@gmail.com>
2022-06-26 13:59:09 +0200

    Netflix Eureka Discovery Server & Clients (#1)
    * configure netflix eureka server; change ports
    
    * configure eureka discovery clients; add random ports for services
    
    Co-authored-by: Bartlomiej Pabisiak <bartlomiej.pabisiak@gmail.com>

--- dental-clinic-discovery-server/src/main/java/com/microservices/dental/clinic/discovery/server/DiscoveryServerApplication.java
+++ dental-clinic-discovery-server/src/main/java/com/microservices/dental/clinic/discovery/server/DiscoveryServerApplication.java
@@ -2,7 +2,9 @@ package com.microservices.dental.clinic.discovery.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
