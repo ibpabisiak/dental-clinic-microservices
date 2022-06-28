cdb9ad7202079f11c0ec9b001dfa9506eee100f9
Bartlomiej Pabisiak <ibpabisiak@gmail.com>
2022-06-28 11:56:40 +0200

    Introduction of Spring Cloud Gateway  (#3)
    * add DentistsController - returns dummy data
    
    * configure Spring Cloud Gateway

---
+++ dental-clinic-dentists-service/src/main/java/com/microservices/dental/clinic/dentists/service/DentistsService.java
@@ -0,0 +1,32 @@
+package com.microservices.dental.clinic.dentists.service;
+
+import com.microservices.dental.clinic.dentists.service.data.DentistDTO;
+import com.microservices.dental.clinic.dentists.service.data.DentistSpeciality;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class DentistsService {
+
+    public List<DentistDTO> getAllDentists() {
+        //TODO implement loading from database
+        return prepareDummyData(5);
+    }
+
+
+    private List<DentistDTO> prepareDummyData(int count) {
+        var dentists = new ArrayList<DentistDTO>();
+        for (int i = 0; i < count; i++) {
+            dentists.add(
+                new DentistDTO(UUID.randomUUID().toString(), "John" + i, "Smith", Instant.now(),
+                    DentistSpeciality.ENDODONTIST));
+        }
+        return dentists;
+    }
+
+}
