426a70cfeaffa0ba54505e7d03f41b726121157e
Bartlomiej Pabisiak <bartlomiej.pabisiak@gmail.com>
2022-12-10 14:18:55 +0100

    update db schema and initial data

---
+++ dental-clinic-visits-service/src/main/resources/db/hsqldb/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO visits VALUES (1, 2, 3, null, 'desc');
+INSERT INTO visits VALUES (2, 3, 4, null, 'desc2');
--- dental-clinic-visits-service/src/main/resources/hsqldb/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO visits VALUES (1, '21-04-2022', 'temporary description', 1, 2);
-INSERT INTO visits VALUES (2, '21-04-2022', 'temporary description 2', 2, 3);
